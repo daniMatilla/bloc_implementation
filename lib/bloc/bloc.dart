fcbae8d1ff83b7c2d7d2a0137c3ed6fee01e1a80
daniMatilla <danimatilla@outlook.es>
2022-06-04 14:00:47 +0200

    Captura de errores

--- lib/bloc/bloc.dart
+++ lib/bloc/bloc.dart
@@ -20,17 +20,31 @@ abstract class Bloc<Event, State> {
     _stateSubject.close();
   }
 
+  void onError(Object error, StackTrace stackTrace);
+
   void dispatch(Event event) {
-    _eventSubject.sink.add(event);
+    try {
+      _eventSubject.sink.add(event);
+    } on Exception catch (error) {
+      _handlerError(error);
+    }
   }
 
   Stream<State> mapEventToState(Event event);
 
   void _bindStateSubject() {
-    _eventSubject.asyncExpand(mapEventToState).forEach(
+    _eventSubject
+        .asyncExpand(
+            (Event event) => mapEventToState(event).handleError(_handlerError))
+        .forEach(
       (State nextState) {
+        if (currentState == nextState || _stateSubject.isClosed) return;
         _stateSubject.sink.add(nextState);
       },
     );
+  }
+
+  void _handlerError(Object error, [StackTrace stackTrace = StackTrace.empty]) {
+    onError(error, stackTrace);
   }
 }
